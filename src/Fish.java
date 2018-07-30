d1dc0d68228a925b491eb43ab16fab678d73ead9
Sabina Shrestha <sabina.pari@yahoo.com>
2018-07-30 01:07:18 -0600

    fish is swimming

--- src/Fish.java
+++ src/Fish.java
@@ -1,4 +1,7 @@
 
 public class Fish {
 
+	public void swim() {
+		System.out.println("swimming...");
+	}
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -4,10 +4,12 @@ public class Zoo {
 	public static void main(String args[]) {
 		Animal cow = new Animal("Cow", 4, "female", 80, "MOO..");
 		Animal horse = new Animal("Horse", 5, "Male", 60, "Neigh...");
+		Fish fish1 = new Fish();
 		
 		
 		cow.speak();
 		horse.speak();
+		fish1.swim();
 		
 	}
 }
