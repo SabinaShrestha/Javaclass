75e3f3bcd6d99f39486f09eb74ef91d1fc9fcff6
Sabina Shrestha <sabina.pari@yahoo.com>
2018-07-30 01:09:34 -0600

    add bird

--- src/Zoo.java
+++ src/Zoo.java
@@ -5,11 +5,13 @@ public class Zoo {
 		Animal cow = new Animal("Cow", 4, "female", 80, "MOO..");
 		Animal horse = new Animal("Horse", 5, "Male", 60, "Neigh...");
 		Fish fish1 = new Fish();
+		Bird sparrow = new Bird();
 		
 		
 		cow.speak();
 		horse.speak();
 		fish1.swim();
+		sparrow.fly();
 		
 	}
 }
